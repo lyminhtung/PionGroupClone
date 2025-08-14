c75cf747f0cccff647c18cd5ddd6a431c6d56af0
lyminhtung <lyminhtung08@gmail.com>
2025-08-14 17:17:40 +0700

    optimize button component

--- src/components/ui/btn/Button.component.tsx
+++ src/components/ui/btn/Button.component.tsx
@@ -19,6 +19,8 @@ export const Button: React.FC<ButtonProps> = ({
     };
 
     const textClass = sizeToTextClass[size] || 'text-base';
+    const spanDefaultClasses = 'font-semibold tracking-wide text-white pr-2 whitespace-nowrap';
+    const iconDefaultClasses = 'text-white';
 
     const classes = [
         'btn',
@@ -44,10 +46,18 @@ export const Button: React.FC<ButtonProps> = ({
                 {iconPosition === 'left' && renderIcon()}
                 {
                     React.Children.map(children, child => {
-                        // Kiểm tra nếu là một phần tử React hợp lệ trước khi clone
                         if (React.isValidElement(child)) {
-                            return React.cloneElement(child, {
-                                className: `${child.props.className || ''} ${textClass}`.trim(),
+                            let defaultClasses = '';
+                            if (child.type === 'span') {
+                                defaultClasses = spanDefaultClasses;
+                            } else if (child.type === 'i') {
+                                defaultClasses = iconDefaultClasses;
+                            }
+                            const typedChild = child as React.ReactElement<{ className?: string }>;
+                            const existingClasses = typedChild.props.className || '';
+
+                            return React.cloneElement(typedChild, {
+                                className: `${defaultClasses} ${existingClasses} ${textClass}`.trim(),
                             });
                         }
                         return child;
