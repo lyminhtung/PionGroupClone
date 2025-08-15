c93af3c0446a1cc0a6be1eb1e3a589106f3177f9
lyminhtung <lyminhtung08@gmail.com>
2025-08-15 13:19:03 +0700

    Create Board content 04, CardService Component

---
+++ src/components/ui/cards/ServicesCard/ServiceCard.type.ts
@@ -0,0 +1,7 @@
+import { BaseProps } from "@/types/common";
+
+export interface ServiceCardProps extends BaseProps{
+    icon?:React.ReactNode
+    title?: React.ReactNode,
+    description?: string
+}
